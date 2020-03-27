5684511bf3487be84c2bbc174d100e7ef453e920
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-03-27 16:55:22 +0100

    feat(auth): added dummy function

---
+++ services/auth/get.js
@@ -0,0 +1,10 @@
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({message: 'Hello world !'})
+  };
+}
