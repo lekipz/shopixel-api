4c9a6a88ddc38b2760c53f273f746a50449724d6
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-04-02 14:55:58 +0200

    feat(ssm): added ssm client with local configuration

--- services/auth/get.js
+++ services/auth/get.js
@@ -1,5 +1,7 @@
 import {ok} from '../../lib/responses';
+import ssmConfigClient from '../../lib/ssm';
 
 export async function handler(event, context) {
+  const dbUrl = await ssmConfigClient.getByKey('mongoUrl', {withDecryption: true});
   return ok({message: 'Hello world !'});
 }
