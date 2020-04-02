24b7cfdd1e6e635c02d6b5bc7a2c705f2c19e087
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-04-02 17:39:10 +0200

    feat(register): added register endpoint

--- services/auth/get.js
+++
@@ -1,9 +0,0 @@
-import {ok} from '../../lib/responses';
-import {connectToDB} from '../../lib/db';
-
-export async function handler(event, context) {
-  context.callbackWaitsForEmptyEventLoop = false;
-  await connectToDB();
-
-  return ok({message: 'Hello world !'});
-}
