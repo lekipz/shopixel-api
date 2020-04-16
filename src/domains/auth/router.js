219d8090d1462036040cf5e2b574710bbecbc4d2
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-04-16 15:57:48 +0200

    feat(login): added route

--- src/domains/auth/router.js
+++ src/domains/auth/router.js
@@ -5,5 +5,6 @@ import handleRequest from '../../lib/responses/response-wrapper';
 const router = express.Router();
 
 router.post('/register', handleRequest(({body}) => AuthController.register(body)));
+router.post('/login', handleRequest(({body}) => AuthController.login(body)));
 
 export const AuthRoutes = router;
