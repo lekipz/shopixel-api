48ab5da2facd4c3b6e6c313af31132113934e81d
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-04-16 17:04:55 +0200

    feat(login): updated jwt sign to be asynchronous

--- src/lib/security/jwt.js
+++ src/lib/security/jwt.js
@@ -1,8 +1,16 @@
 import JWT from 'jsonwebtoken';
 
 export async function generate(email) {
-  return JWT.sign({}, process.env.SHOPIXEL_JWT_SECRET, {
-    subject: email,
-    expiresIn: '3h'
+  return new Promise((resolve, reject) => {
+    JWT.sign({}, process.env.SHOPIXEL_JWT_SECRET, {
+      subject: email,
+      expiresIn: '3h'
+    }, (err, token) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(token);
+      }
+    });
   });
 }
