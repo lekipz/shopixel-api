77c9140d85b7ed195817e4a212ebef1baaa97382
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-05-07 15:52:43 +0200

    feat(auth): now generate profile on register and add profile name in jwt on login

--- src/domains/customer/router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as CustomerController from './controller';
-import handleRequest from '../../lib/responses/response-wrapper';
-
-const router = express.Router();
-
-router.get('/dummy', handleRequest(() => CustomerController.testGenerateProfile()));
-
-export const CustomerRoutes = router;
