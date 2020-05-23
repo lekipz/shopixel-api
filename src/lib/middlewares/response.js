ce91844609ab164924eab4e952d3ad7162c40536
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-05-23 16:01:09 +0200

    fix(express): disabled all caching

--- src/lib/middlewares/response.js
+++ src/lib/middlewares/response.js
@@ -4,6 +4,11 @@ import InternalServerError from '../responses/statuses/server-error';
 export default function responseMiddleware(err, req, res, next) {
   if (err instanceof HttpResponse) {
     res.status(err.status);
+
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+
     if (err.status === 204) {
       return res.end();
     } else {
