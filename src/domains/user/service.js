fa631063479ad3299dfc8474514c3589f69db7cc
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-06-10 15:08:54 +0200

    feat(recommendations): switch from kinesis to kafka

--- src/domains/user/service.js
+++ src/domains/user/service.js
@@ -2,7 +2,7 @@ import User from './model';
 import {hash} from '../../lib/security/bcrypt';
 import {getRandomProfile} from '../customer/service';
 import * as RecommendationService from '../recommendation/service';
-import {putRecord} from '../../lib/kinesis';
+import {publishMessage} from '../../lib/kafka';
 
 export async function create(body) {
   const user = new User(body);
@@ -26,5 +26,5 @@ export async function findRandomUser() {
 
 export async function startRecommendationsGeneration(data) {
   await RecommendationService.deleteByUserId(data.user);
-  return putRecord(process.env.KINESIS_RECOMMENDATIONS_STREAM_NAME, data.user, JSON.stringify(data));
+  publishMessage(process.env.SHOPIXEL_RECOMMENDATIONS_KAFKA_TOPIC, data);
 }
---
+++ src/lib/kafka.js
@@ -0,0 +1,32 @@
+import Kafka from 'kafka-node';
+
+let client, producer;
+
+async function getKafkaProducer() {
+  if (!producer) {
+    client = new Kafka.KafkaClient({
+      kafkaHost: process.env.SHOPIXEL_KAFKA_HOST
+    });
+    producer = new Kafka.Producer(client);
+
+    return new Promise(resolve => {
+      producer.on('ready', () => resolve(producer));
+    });
+  }
+
+  return producer;
+}
+
+export function publishMessage(topic, message) {
+  if (process.env.NODE_ENV !== 'production') {
+    console.debug('Not sending message to Kafka because running locally.');
+    return;
+  }
+
+  getKafkaProducer().then(producer => {
+    producer.send({
+      topic,
+      messages: message
+    })
+  });
+}
